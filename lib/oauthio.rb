ae0c8efd4cf22cb63bf810e9f88c9e459d0a303f
Dimitri Jorge <jorge.dimitri@gmail.com>
2017-12-29 11:49:05 +0100

    Implement some logging helpers

--- lib/oauthio.rb
+++ lib/oauthio.rb
@@ -1,3 +1,5 @@
+require 'logger'
+
 require "oauthio/version"
 
 module Oauthio
@@ -7,14 +9,43 @@ module Oauthio
   @oauthd_url = 'https://oauth.io',
   @oauthd_base = '/auth'
 
+  @log_level = nil
+  @logger = nil
+
+  # map to the same values as the standard library's logger
+  LEVEL_DEBUG = Logger::DEBUG
+  LEVEL_ERROR = Logger::ERROR
+  LEVEL_INFO = Logger::INFO
+
   class << self
     attr_accessor :csrf_tokens
 
-    attr_reader :public_key, :secret_key
+    attr_reader :public_key, :secret_key, :log_level
   end
 
   def self.set_credentials pk, sk
     @public_key = pk
     @secret_key = sk
   end
+
+  def self.log_level
+    @log_level
+  end
+
+  def self.log_level=(val)
+    if !val.nil? && ![LEVEL_DEBUG, LEVEL_ERROR, LEVEL_INFO].include?(val)
+      raise ArgumentError, "log_level should only be set to `nil`, `debug` or `info`"
+    end
+    @log_level = val
+  end
+
+
+  def self.logger
+    @logger
+  end
+
+  def self.logger=(val)
+    @logger = val
+  end
+
 end
--- spec/oauthio_spec.rb
+++ spec/oauthio_spec.rb
@@ -1,15 +1,48 @@
-require 'pry'
-require 'oauthio'
+require 'spec_helper'
 
 RSpec.describe Oauthio do
   it "has a version number" do
     expect(Oauthio::VERSION).not_to be nil
   end
 
-  it 'has credentials' do
+  it '.set_credentials' do
     Oauthio.set_credentials('xxx', 'bbb')
 
     expect(Oauthio.public_key).to eq 'xxx'
     expect(Oauthio.secret_key).to eq 'bbb'
+  end
+
+  describe '.log_level' do
+    it 'has a default value' do
+      expect(Oauthio.log_level).to eq nil
+    end
+
+    it 'can be set' do
+      [Oauthio::LEVEL_DEBUG, Oauthio::LEVEL_ERROR, Oauthio::LEVEL_INFO, nil].each do |val|
+        Oauthio.log_level = val
+        expect(Oauthio.log_level).to eq val
+      end
+    end
+
+    it 'cannot be anything' do
+      ['', 'abcd', 4242].each do |invalid_value|
+        expect {
+          Oauthio.log_level = invalid_value
+        }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '.logger' do
+    it 'is not set by default' do
+      expect(Oauthio.logger).to eq nil
+    end
+
+    it 'can be set' do
+      logger = ::Logger.new(StringIO.new)
+      Oauthio.logger = logger
+
+      expect(Oauthio.logger).to eq logger
+    end
   end
 end
