cf6575970f3ac1367c02f18c7152c758a75f0d11
Dimitri Jorge <jorge.dimitri@gmail.com>
2017-12-29 11:27:10 +0100

    Add set_credentials

--- lib/oauthio.rb
+++ lib/oauthio.rb
@@ -1,5 +1,20 @@
 require "oauthio/version"
 
 module Oauthio
-  # Your code goes here...
+  @public_key = nil
+  @secret_key = nil
+  @csrf_tokens = [],
+  @oauthd_url = 'https://oauth.io',
+  @oauthd_base = '/auth'
+
+  class << self
+    attr_accessor :csrf_tokens
+
+    attr_reader :public_key, :secret_key
+  end
+
+  def self.set_credentials pk, sk
+    @public_key = pk
+    @secret_key = sk
+  end
 end
--- spec/oauthio_spec.rb
+++ spec/oauthio_spec.rb
@@ -1,9 +1,15 @@
+require 'pry'
+require 'oauthio'
+
 RSpec.describe Oauthio do
   it "has a version number" do
     expect(Oauthio::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'has credentials' do
+    Oauthio.set_credentials('xxx', 'bbb')
+
+    expect(Oauthio.public_key).to eq 'xxx'
+    expect(Oauthio.secret_key).to eq 'bbb'
   end
 end
